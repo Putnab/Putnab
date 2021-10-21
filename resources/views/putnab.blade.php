6468af604b957d2f96696d2a4a41bbc6fd90644d
Putnab <putrinabilah1326@gmail.com>
2021-10-21 08:49:16 +0700

    tugas 2: admin template

---
+++ resources/views/putnab.blade.php
@@ -0,0 +1,16 @@
+@extends('ddashboard.dashboard')
+
+@section('content')
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0">putnab</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                </div><!-- /.col -->
+              </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+          </div>
+@endsection
